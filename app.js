29eb4edce307d7104d0eb3713eb131d8d54a5543
vuly <phuongoanh0795@gmail.com>
2018-08-05 16:14:46 +0700

    Change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/forex");
+mongoose.connect("mongodb://127.0.0.1:27017/forex");
 
 //schema room
 const roomSchema = new mongoose.Schema({
